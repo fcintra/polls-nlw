bf0d987649ff3f85d670ca54b09394a8889ee4fe
fcintra <matheus.cintra_2002@hotmail.com>
2024-02-07 21:07:16 -0300

    feat: routes and model vote

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,20 @@
+import cookie from "@fastify/cookie";
 import fastify from "fastify";
 import { createPoll } from "./routes.ts/create-poll";
+import { getPoll } from "./routes.ts/get-poll";
+import { voteOnPoll } from "./routes.ts/vote-on-poll";
 const app = fastify();
 
+app.register(cookie, {
+    secret: "polls-app",
+    hook: 'onRequest',
+    parseOptions: {}
+})
 
 app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server is running')
-})
+});
